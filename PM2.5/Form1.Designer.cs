e6ffc9772123f6b50e0311711d74819021b1c8fe
Yu <jiwjexample.com>
2018-03-22 18:15:00 +0800

    20180322

--- PM2.5/Form1.Designer.cs
+++ PM2.5/Form1.Designer.cs
@@ -28,12 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label_lng = new System.Windows.Forms.Label();
             this.label_lat = new System.Windows.Forms.Label();
             this.label_alt = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // label1
@@ -95,11 +98,22 @@
             this.label_alt.Size = new System.Drawing.Size(0, 19);
             this.label_alt.TabIndex = 8;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(56, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(307, 228);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label_alt);
             this.Controls.Add(this.label_lat);
             this.Controls.Add(this.label_lng);
@@ -121,6 +135,8 @@
         private System.Windows.Forms.Label label_lng;
         private System.Windows.Forms.Label label_lat;
         private System.Windows.Forms.Label label_alt;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- PM2.5/Form1.cs
+++ PM2.5/Form1.cs
@@ -8,51 +8,76 @@ using System.Text;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
 using System.Windows.Forms;
+using System.Threading;
+using System.Timers;
 
 namespace PM2._5
 {
     public partial class Form1 : Form
     {
-        //static MySqlDataReader myData;
         public Form1()
         {
             InitializeComponent();
         }
-
         private void Form1_Load(object sender, EventArgs e)
         {
-            mainloop();
-        }     
-        private void mainloop()
+
+        }
+        private void Mainloop()
         {
-            string dbHost = "140.130.20.168";   //資料庫位址
-            string dbUser = "gpspm2.5";         //資料庫使用者帳號
-            string dbPass = "gpspm2.5";         //資料庫使用者密碼
-            string dbName = "gpspm25";          //資料庫名稱
-
-            string connStr = "server=" + dbHost + ";uid=" + dbUser + ";pwd=" + dbPass + ";database=" + dbName;
-            MySqlConnection conn = new MySqlConnection(connStr);
-
-            // 進行select
-            string SQL = "select * from test1 order by id desc limit 0,1 ";
-            try
+            DateTime Repload = DateTime.Now;
+            while (true)
             {
-                conn.Open();
-                MySqlCommand cmd = new MySqlCommand(SQL, conn);
-                MySqlDataReader myData = cmd.ExecuteReader();
-
-                while (myData.Read())
+                //if (Repload.AddSeconds(1) <= DateTime.Now)
                 {
-                    label_lng.Text = myData.GetString(2);
-                    label_lat.Text = myData.GetString(3);
-                    label_alt.Text = myData.GetString(4);
+                    string dbHost = "140.130.20.168";   //資料庫位址
+                    string dbUser = "gpspm2.5";         //資料庫使用者帳號
+                    string dbPass = "gpspm2.5";         //資料庫使用者密碼
+                    string dbName = "gpspm25";          //資料庫名稱
+
+                    string connStr = "server=" + dbHost + ";uid=" + dbUser + ";pwd=" + dbPass + ";database=" + dbName;
+                    MySqlConnection conn = new MySqlConnection(connStr);
+
+                    // 進行select
+                    string SQL = "select * from test1 order by id desc limit 0,1 ";
+
+                    if (Repload.AddSeconds(1) < DateTime.Now)
+                    {
+                        try
+                        {
+                            conn.Open();
+                            MySqlCommand cmd = new MySqlCommand(SQL, conn);
+                            MySqlDataReader myData = cmd.ExecuteReader();
+
+                            while (myData.Read())
+                            {
+                                label_lng.Text = myData.GetString(2);
+                                label_lat.Text = myData.GetString(3);
+                                label_alt.Text = myData.GetString(4);
+                                //myData.Read();
+                            }
+                            //myData.Close();
+                        }
+                        catch (MySql.Data.MySqlClient.MySqlException ex)
+                        {
+                            Console.WriteLine("Error " + ex.Number + " : " + ex.Message);
+                        }
+                        Repload = DateTime.Now;
+                    }
                 }
-                myData.Close();
-            }
-            catch (MySql.Data.MySqlClient.MySqlException ex)
-            {
-                //Console.WriteLine("Error " + ex.Number + " : " + ex.Message);
             }
         }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //new Thread(Mainloop) { IsBackground = true }.Start();
+            Mainloop();
+        }
+        //public void timer1_Tick()
+        //{
+        //    System.Windows.Forms.Timer timer1 = new System.Windows.Forms.Timer();
+        //    timer1.Tick += new EventHandler(Mainloop);
+        //    timer1.Interval = 1000;
+        //    timer1.Start();
+        //}
     }
 }
